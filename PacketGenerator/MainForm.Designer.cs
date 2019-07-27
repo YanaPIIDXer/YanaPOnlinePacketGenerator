a2917a09a7eeec29455b5dda5e95ff12d3528c4a
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2019-07-27 15:46:04 +0900

    プロジェクトを新規作成できるようにした。 ※とりあえずディレクトリを掘るだけ。

--- PacketGenerator/MainForm.Designer.cs
+++ PacketGenerator/MainForm.Designer.cs
@@ -28,13 +28,36 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.ProjectListBox = new System.Windows.Forms.ListBox();
+			this.CreateProjectButton = new System.Windows.Forms.Button();
 			this.SuspendLayout();
+			// 
+			// ProjectListBox
+			// 
+			this.ProjectListBox.FormattingEnabled = true;
+			this.ProjectListBox.ItemHeight = 15;
+			this.ProjectListBox.Location = new System.Drawing.Point(46, 23);
+			this.ProjectListBox.Name = "ProjectListBox";
+			this.ProjectListBox.Size = new System.Drawing.Size(277, 364);
+			this.ProjectListBox.TabIndex = 0;
+			// 
+			// CreateProjectButton
+			// 
+			this.CreateProjectButton.Location = new System.Drawing.Point(99, 394);
+			this.CreateProjectButton.Name = "CreateProjectButton";
+			this.CreateProjectButton.Size = new System.Drawing.Size(167, 44);
+			this.CreateProjectButton.TabIndex = 1;
+			this.CreateProjectButton.Text = "プロジェクト新規作成";
+			this.CreateProjectButton.UseVisualStyleBackColor = true;
+			this.CreateProjectButton.Click += new System.EventHandler(this.CreateProjectButton_Click);
 			// 
 			// MainForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.CreateProjectButton);
+			this.Controls.Add(this.ProjectListBox);
 			this.Name = "MainForm";
 			this.Text = "PacketGenerator";
 			this.Load += new System.EventHandler(this.MainForm_Load);
@@ -43,6 +66,9 @@
 		}
 
 		#endregion
+
+		private System.Windows.Forms.ListBox ProjectListBox;
+		private System.Windows.Forms.Button CreateProjectButton;
 	}
 }
 
--- PacketGenerator/MainForm.cs
+++ PacketGenerator/MainForm.cs
@@ -22,6 +22,7 @@ namespace PacketGenerator
 		public MainForm()
 		{
 			InitializeComponent();
+			ReloadProjectList();
 		}
 
 		// フォームが読み込まれた
@@ -30,6 +31,40 @@ namespace PacketGenerator
 			// プロジェクトデータを放り込むディレクトリが生成されていなければ生成する。
 			// 初回起動時の処理。
 			Directory.CreateDirectory(Config.ProjectRootDir);
+		}
+
+		// プロジェクト新規作成ボタンが押された
+		private void CreateProjectButton_Click(object sender, EventArgs e)
+		{
+			CreateProjectDialog Dialog = new CreateProjectDialog();
+			var Result = Dialog.ShowDialog();
+			if(Result != DialogResult.OK) { return; }
+
+			var Path = Config.ProjectRootDir + Dialog.ProjectName;
+			if(Directory.Exists(Path))
+			{
+				MessageBox.Show(Dialog.ProjectName + "は既に存在します。");
+				return;
+			}
+
+			Directory.CreateDirectory(Path);
+			ReloadProjectList();
+
+			MessageBox.Show("プロジェクトを作成しました。");
+		}
+
+		/// <summary>
+		/// プロジェクトリストのリロード
+		/// </summary>
+		private void ReloadProjectList()
+		{
+			ProjectListBox.Items.Clear();
+
+			var Projects = Directory.GetDirectories(Config.ProjectRootDir, "*", SearchOption.TopDirectoryOnly);
+			foreach(var Project in Projects)
+			{
+				ProjectListBox.Items.Add(Path.GetFileNameWithoutExtension(Project));
+			}
 		}
 	}
 }
