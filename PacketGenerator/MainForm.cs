d0ce3b58eaf6c09ec61928f6d48b3e69f8bf7134
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2019-07-27 15:27:07 +0900

    ・.gitignoreの追加。 ・初回起動時にプロジェクトデータを放り込むディレクトリを自動生成するようにした。

--- PacketGenerator/MainForm.cs
+++ PacketGenerator/MainForm.cs
@@ -7,14 +7,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace PacketGenerator
 {
+	/// <summary>
+	/// メインフォーム
+	/// </summary>
 	public partial class MainForm : Form
 	{
+		/// <summary>
+		/// コンストラクタ
+		/// </summary>
 		public MainForm()
 		{
 			InitializeComponent();
+		}
+
+		// フォームが読み込まれた
+		private void MainForm_Load(object sender, EventArgs e)
+		{
+			// プロジェクトデータを放り込むディレクトリが生成されていなければ生成する。
+			// 初回起動時の処理。
+			Directory.CreateDirectory(Config.ProjectRootDir);
 		}
 	}
 }
